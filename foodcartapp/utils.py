a558c49e32f24cc3bd127a4e92b8ab67cacd8593
Raman <romansashin04@gmail.com>
2025-06-14 14:31:52 +0300

    Функция чтобы расстояние бралось из БД

--- foodcartapp/utils.py
+++ foodcartapp/utils.py
@@ -1,24 +1,34 @@
 import requests
 from geopy.distance import distance as geopy_distance
 from django.conf import settings
+from places.models import Place
 
 def fetch_coordinates(address):
-    apikey = settings.YANDEX_GEOCODER_API_KEY
+    place, created = Place.objects.get_or_create(address=address)
+
+    if place.latitude and place.longitude:
+        return place.latitude, place.longitude
+
     url = "https://geocode-maps.yandex.ru/1.x/"
-    params = {"apikey": apikey, "geocode": address, "format": "json"}
+    params = {
+        "apikey": settings.YANDEX_GEOCODER_API_KEY,
+        "geocode": address,
+        "format": "json",
+    }
 
     try:
         response = requests.get(url, params=params)
         response.raise_for_status()
-    except requests.RequestException:
+        geo_data = response.json()
+        geo_object = geo_data['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
+        lon, lat = map(float, geo_object['Point']['pos'].split())
+        place.latitude = lat
+        place.longitude = lon
+        place.save()
+        return lat, lon
+    except (KeyError, IndexError, ValueError, requests.RequestException):
         return None
 
-    try:
-        geo_object = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']
-        lon, lat = map(float, geo_object['Point']['pos'].split(' '))
-        return lat, lon
-    except (IndexError, KeyError, ValueError):
-        return None
 
 def get_distance_km(from_coords, to_coords):
     return round(geopy_distance(from_coords, to_coords).km, 2)
